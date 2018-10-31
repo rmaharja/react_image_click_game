5808ea9b2f383f6e1bf803d7deffcaa3653cf294
rmaharja@gmail.com <rmaharja@gmail.com>
2018-10-31 15:37:01 -0400

    Updates

---
+++ precache-manifest.a5741b18c415d2319dbdb680c4fc1447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23e3588ea895aa97d767",
+    "url": "/react_image_click_game/static/js/runtime~main.23e3588e.js"
+  },
+  {
+    "revision": "c817a528770d81855def",
+    "url": "/react_image_click_game/static/js/main.c817a528.chunk.js"
+  },
+  {
+    "revision": "7ccad06e8a96e8ae6253",
+    "url": "/react_image_click_game/static/js/1.7ccad06e.chunk.js"
+  },
+  {
+    "revision": "c817a528770d81855def",
+    "url": "/react_image_click_game/static/css/main.caf68a9f.chunk.css"
+  },
+  {
+    "revision": "0cf123334502f4f63965e8373a39bbda",
+    "url": "/react_image_click_game/index.html"
+  }
+];
